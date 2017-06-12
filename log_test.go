ff63def94e492b3246ca172e026f8772cbfc51ad
Eduard Urbach <e.urbach@gmail.com>
2017-06-12 02:26:07 +0200

    Removed unused bench function

--- log_test.go
+++ log_test.go
@@ -1,33 +1,10 @@
 package log
 
 import (
-	"runtime"
-	"sync"
 	"testing"
 
 	"go.uber.org/zap"
 )
-
-func bench(b *testing.B, fun func()) {
-	b.ReportAllocs()
-
-	wg := sync.WaitGroup{}
-	tasks := runtime.NumCPU()
-	wg.Add(tasks)
-
-	b.ResetTimer()
-
-	for i := 0; i < tasks; i++ {
-		go func() {
-			for n := 0; n < b.N; n++ {
-				fun()
-			}
-
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-}
 
 func BenchmarkAeroLog(b *testing.B) {
 	web := NewChannel("web")
